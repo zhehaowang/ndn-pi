15829517c678ca00bec2624807422469d5d7fb30
zhehaowang <wangzhehao410305@gmail.com>
2015-07-04 17:31:02 -0700

    openloop-publisher-example: add openloop publisher

--- examples/openloop_publisher/publisher.py
+++ examples/openloop_publisher/publisher.py
@@ -4,7 +4,19 @@
 # This is the data publisher using MemoryContentCache
 #
 
-from pyndn import Name, Face, Interest, Data
+# remove Adeola's version of PyNDN in sys.path
+# this would also require
+# export PYTHONPATH=$PYTHONPATH:~/zhehao-test/pyndn2/python
+
+# this test does not expect prefix registered on the remote nfd that we connect to...
+#import sys
+#if ("/usr/local/lib/python2.7/dist-packages/PyNDN-2.0a3-py2.7.egg" in sys.path):
+#    sys.path.remove("/usr/local/lib/python2.7/dist-packages/PyNDN-2.0a3-py2.7.egg")
+#    sys.path.append("/home/pi/zhehao-test/pyndn2/python")
+
+# Updating PyNDN could come at a later time.
+
+from pyndn import Name, Face, Interest, Data, ThreadsafeFace
 from pyndn.util.memory_content_cache import MemoryContentCache
 from pyndn.transport.udp_transport import UdpTransport
 
@@ -15,9 +27,9 @@ import json
 from ndn_pi.iot_node import IotNode
 
 import logging
-class CachedContentPublisher(IotNode):
+class OpenloopPublisher(IotNode):
     def __init__(self, transport, conn):
-        super(CachedContentPublisher, self).__init__(transport, conn)
+        super(OpenloopPublisher, self).__init__(transport, conn)
         self._missedRequests = 0
         self._dataPrefix = None
         self.addCommand(Name('listPrefixes'), self.listDataPrefixes, ['repo'],
@@ -25,9 +37,9 @@ class CachedContentPublisher(IotNode):
 
     def setupComplete(self):
         # The cache will clear old values every 100s
-        self._dataCache = MemoryContentCache(self.face, 100000)
+        #self._dataCache = MemoryContentCache(self.face, 100000)
         self._dataPrefix = Name(self.prefix).append('data')
-        self.registerCachePrefix()
+        #self.registerCachePrefix()
         print "Serving data at {}".format(self._dataPrefix.toUri())
         self.loop.call_soon(self.publishData)
 
@@ -38,8 +50,8 @@ class CachedContentPublisher(IotNode):
         d.getMetaInfo().setFreshnessPeriod(10000)
         return d
 
-    def registerCachePrefix(self):
-        self._dataCache.registerPrefix(self._dataPrefix, self.cacheRegisterFail , self.onDataMissing)
+    #def registerCachePrefix(self):
+    #    self._dataCache.registerPrefix(self._dataPrefix, self.cacheRegisterFail , self.onDataMissing)
 
     def unknownCommandResponse(self, interest):
         # we override this so the MemoryContentCache can handle data requests
@@ -47,12 +59,12 @@ class CachedContentPublisher(IotNode):
         if afterPrefix == 'data':
             return None
         else:
-            return super(CachedContentPublisher, self).unknownCommandResponse(interest)
+            return super(OpenloopPublisher, self).unknownCommandResponse(interest)
 
-    def cacheRegisterFail(self, interest):
+    #def cacheRegisterFail(self, interest):
         # just try again
-        self.log.warn('Could not register data cache')
-        self.registerCachePrefix()
+    #    self.log.warn('Could not register data cache')
+    #    self.registerCachePrefix()
 
     def onDataMissing(self, prefix, interest, transport, prefixId):
         self._missedRequests += 1
@@ -74,14 +86,23 @@ class CachedContentPublisher(IotNode):
         dataOut.getMetaInfo().setFreshnessPeriod(10000)
         self.signData(dataOut)
 
-        self._dataCache.add(dataOut)
+        #self._dataCache.add(dataOut)
+        # instead of adding data to content cache, we put data to nfd anyway
+        self.send(dataOut.wireEncode().buf())
+        print('data name: ' + dataOut.getName().toUri())
 
-        # repeat every 5 seconds
-        self.loop.call_later(5, self.publishData)
+        # repeat every 1 seconds
+        self.loop.call_later(1, self.publishData)
+
+    def send(self, encoding):
+        # Interesting, yet I thought self.face was a threadsafe face.
+        self.face._node._transport.send(encoding)
+        #self.face._loop.call_soon_threadsafe(
+        #    super(ThreadsafeFace, self.face)._node._transport.send, encoding)
 
 if __name__ == '__main__':
     transport = UdpTransport()
     # TODO: This multicast address should read from nfd configuration
     conn = UdpTransport.ConnectionInfo("224.0.23.170")
-    n = CachedContentPublisher(transport, conn)
+    n = OpenloopPublisher(transport, conn)
     n.start()
